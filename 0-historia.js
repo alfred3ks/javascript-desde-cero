533d1398329b0186a18c061382b5e4816621643e
Alfredo Sánchez <alfred3ks@gmail.com>
2022-09-30 04:17:53 +0200

    La historia de JS y como correr su codigo.

---
+++ 0-historia.js
@@ -0,0 +1,52 @@
+/*
+
+Video - 0:
+
+La historia de JS:
+
+- JS es un lenguaje de programacion,
+- Lo creo Brendan Eich en una semana,
+- Es el unico lenguaje de programacion que puede interpretar el navegador.
+- El nombre se le puso porque estaba de moda Java.
+- JavaScript ha sido mal visto desde su inicio hasta el 2009, cuando ha evolucionado a mejor.
+- Hasta el 2009 se podia utilizar solo del lado del cliente. Actualmente con NodeJS podemos ejecutarlo en un servidor.
+- 1995 - Netscape crea JavaScript.
+- 1997 - Netscape se leva a JAvaScript a ECMA, (European Computer Manufactures Association). Son los que regulan el standart de JS a dia de hoy.
+- 1997 se acepta y se lanza la especificacion estandar ECMA-262 y se crea ECMAScript 1.0
+- 1998 sale ECMAScript 2, con solo un ajuste para el estandar internacional.
+- 1999 sale ECMAScript 3,se le van añadiendo mas cosas, pero el lenguaje no termina de cuajar.
+-2009 sale ECMAScript 5 co muchas actualizacion que dan a gran salto al lenguaje.
+- 2011 ECMAScript 5.1, pequeñas mejoras.
+- 2015 -ECMAScript 6, aqui llega el gran boom del lenguaje, este es el standart actual del lenguaje.
+
+Tecnologias descendientes de JavaScript.
+Tecnologias independientes:
+- AJAX,
+- JQuery,
+- nodeJS,
+- JSON
+
+Tecnologias dependientes:
+Frameworks:
+- Angular,
+- React,
+- VueJS
+
+Como escribir el codigo de JS:
+
+Se necesita un editor de texto, no un procesador de texto, por lo que word no sirve.
+
+Existen varias alternativas:
+- La consola del navegador directamente.
+- Con NodeJS,
+
+Diversos editores de codigo:
+- Sublime text,
+- Atom,
+- Visual Studio Code,
+- Brackets,
+- Notepad++,
+- Bloc de notas, etc.
+
+
+*/
